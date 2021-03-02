c9941ee0e4c055e85c22762ce99fd96bf0aa9341
Juho Lehtonen <juho.lehtonen@csc.fi>
2021-03-03 01:30:42 +0200

    CSCNKR-207 Fix database reference as per upstream 01bdc3c953ade443

--- src/RecordManager/NKR/Record/Ead3.php
+++ src/RecordManager/NKR/Record/Ead3.php
@@ -25,10 +25,11 @@
  * @author   Jukka Lehmus <jlehmus@mappi.helsinki.fi>
  * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://github.com/KDK-Alli/RecordManager
+ * @link     https://github.com/NatLibFi/RecordManager
  */
 namespace RecordManager\NKR\Record;
 
+use RecordManager\Base\Database\DatabaseInterface as Database;
 use RecordManager\Base\Utils\Logger;
 
 /**
@@ -43,20 +44,19 @@ use RecordManager\Base\Utils\Logger;
  * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
  * @author   Juho Lehtonen <juho.lehtonen@csc.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://github.com/KDK-Alli/RecordManager
+ * @link     https://github.com/NatLibFi/RecordManager
  */
 class Ead3 extends \RecordManager\Finna\Record\Ead3
 {
     /**
      * Return fields to be indexed in Solr
      *
-     * @param \RecordManager\Base\Database\Database $db Database connection. Omit to
-     *                                                  avoid database lookups for
-     *                                                  related records.
+     * @param Database $db Database connection. Omit to avoid database lookups for
+     *                     related records.
      *
      * @return array
      */
-    public function toSolrArray(\RecordManager\Base\Database\Database $db = null)
+    public function toSolrArray(Database $db = null)
     {
         $data = parent::toSolrArray($db);
         $data['_document_id'] = $this->getUnitId();
